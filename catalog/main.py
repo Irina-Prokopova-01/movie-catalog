2f64a4e70feaed341ef1afe6876d220f668226aa
Irina <Irina-prokopova.style@yandex.ru>
2025-09-20 12:19:01 +0300

    Change main.py for code reduction and connect api_router

--- catalog/main.py
+++ catalog/main.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI, HTTPException, status
 from starlette.requests import Request
 from schemas.movie import Movie
 
+from api import router as api_router
 
 app = FastAPI(
     title="Movie Catalog",
 )
+
+app.include_router(api_router)
 
 
 @app.get("/")
@@ -18,44 +21,3 @@ def read_root(request: Request):
         "massage": "Hello World",
         "url": str(url_docs),
     }
-
-
-LIST_MOVIES = [
-    Movie(
-        movie_id=1,
-        title="Movie 1",
-        description="Movie description1",
-        year=2020,
-        rating=4.5,
-    ),
-    Movie(
-        movie_id=2,
-        title="Movie 2",
-        description="Movie description2",
-        year=2025,
-        rating=4.7,
-    ),
-    Movie(
-        movie_id=3,
-        title="Movie 3",
-        description="Movie description3",
-        year=2025,
-        rating=4.8,
-    ),
-]
-
-
-@app.get("/list_movies/", response_model=list[Movie])
-def list_all_movies():
-    return LIST_MOVIES
-
-
-@app.get("/{movie_id}", response_model=Movie)
-def read_movie(movie_id: int):
-    for movie in LIST_MOVIES:
-        if movie.movie_id == movie_id:
-            return movie
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"URL {movie_id!r} not found.",
-    )
