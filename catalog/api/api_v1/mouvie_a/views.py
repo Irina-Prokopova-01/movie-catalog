47faa791ee592d71d23f90f1c3feb8d1ae9c1fe9
Irina <Irina-prokopova.style@yandex.ru>
2025-09-22 11:22:13 +0300

    Change create_movie for JSON and generate id in view (random.randint)

--- catalog/api/api_v1/mouvie_a/views.py
+++ catalog/api/api_v1/mouvie_a/views.py
@@ -4,41 +4,29 @@ import random
 from fastapi import APIRouter, status, Form
 
 from api.api_v1.mouvie_a.crud import LIST_MOVIES
-from schemas.movie import Movie
+from schemas.movie import BaseMovie, CreateMovie, MovieResponse
 
 router = APIRouter(
     prefix="/movies",
     tags=["Movies_list"],
 )
 
+movies: list[BaseMovie] = []
 
-@router.get("/list_movies/", response_model=list[Movie])
+
+@router.get("/list_movies/", response_model=list[BaseMovie])
 def list_all_movies():
-    return LIST_MOVIES
-
-
-movies: list[Movie] = []
+    return movies
 
 
 @router.post(
     "/create_movie/",
-    response_model=Movie,
+    response_model=BaseMovie,
     status_code=status.HTTP_201_CREATED,
 )
-def create_movie(
-    id: Annotated[int, Form()],
-    title: Annotated[str, Len(max_length=20, min_length=3), Form()],
-    description: Annotated[str, Len(max_length=100, min_length=20), Form()],
-    year: Annotated[int, Form()],
-    rating: Annotated[float, Form()],
-) -> Movie:
+def create_movie(movie_create: CreateMovie) -> BaseMovie:
+    # Генерируем случайный id
     movie_id = random.randint(3, 1000)
-    new_movie = Movie(
-        id=movie_id,
-        title=title,
-        description=description,
-        year=year,
-        rating=rating,
-    )
+    new_movie = BaseMovie(id=movie_id, **movie_create.model_dump())
     movies.append(new_movie)
-    return new_movie
+    return BaseMovie(**new_movie.model_dump())
