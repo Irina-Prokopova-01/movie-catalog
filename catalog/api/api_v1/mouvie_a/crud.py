ec41bc51cf42a05eedca8c4dd2bb50f6b3d840e4
Irina <Irina-prokopova.style@yandex.ru>
2025-09-23 13:06:00 +0300

    I use now storage instid of LIST_MOVIES

--- catalog/api/api_v1/mouvie_a/crud.py
+++ catalog/api/api_v1/mouvie_a/crud.py
@@ -1,25 +1,40 @@
-from schemas.movie import BaseMovie
+from pydantic import BaseModel
 
-LIST_MOVIES = [
-    BaseMovie(
-        slug="movie_1",
-        title="Movie 1",
-        description="Movie description1",
-        year=2020,
-        rating=4.5,
-    ),
-    BaseMovie(
-        slug="movie_2",
-        title="Movie 2",
-        description="Movie description2",
-        year=2025,
-        rating=4.7,
-    ),
-    BaseMovie(
+from schemas.movie import BaseMovie, CreateMovie
+
+
+class Storage(BaseModel):
+    slug_movies: dict[str, BaseMovie] = {}
+
+    def get(self) -> list[BaseMovie]:
+        return list(self.slug_movies.values())
+
+    def get_by_slug(self, movie_slug) -> BaseMovie | None:
+        return self.slug_movies.get(movie_slug)
+
+    def create(self, movie_create_new: CreateMovie) -> BaseMovie:
+        new_movie = BaseMovie(**movie_create_new.model_dump())
+        self.slug_movies[new_movie.slug] = new_movie
+        return new_movie
+
+
+storage = Storage()
+
+storage.create(
+    CreateMovie(
         slug="movie_3",
         title="Movie 3",
         description="Movie description3",
         year=2025,
         rating=4.8,
-    ),
-]
+    )
+)
+storage.create(
+    CreateMovie(
+        slug="movie_2",
+        title="Movie 2",
+        description="Movie description2",
+        year=2024,
+        rating=4.7,
+    )
+)
