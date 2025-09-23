43e01225187e63dcb4b00ab057dd4765694ba51c
Irina <Irina-prokopova.style@yandex.ru>
2025-09-23 13:07:42 +0300

    I use now storage and change LIST_MOVIES to storage in create_movie list_all_movie

--- catalog/api/api_v1/mouvie_a/views.py
+++ catalog/api/api_v1/mouvie_a/views.py
@@ -1,35 +1,17 @@
-from typing import Annotated
-from annotated_types import Len
-import random
 from fastapi import APIRouter, status, Form
 
-from api.api_v1.mouvie_a.crud import LIST_MOVIES
-from schemas.movie import BaseMovie, CreateMovie, MovieResponse
+from api.api_v1.mouvie_a.crud import storage
+from schemas.movie import BaseMovie, CreateMovie
 
 router = APIRouter(
     prefix="/movies",
     tags=["Movies_list"],
 )
 
-movies: list[BaseMovie] = []
-
 
 @router.get("/list_movies/", response_model=list[BaseMovie])
-def list_all_movies():
-    return movies
-
-
-# @router.post(
-#     "/create_movie/",
-#     response_model=BaseMovie,
-#     status_code=status.HTTP_201_CREATED,
-# )
-# def create_movie(movie_create: CreateMovie) -> BaseMovie:
-#     # Генерируем случайный id
-#     movie_id = random.randint(3, 1000)
-#     new_movie = BaseMovie(id=movie_id, **movie_create.model_dump())
-#     movies.append(new_movie)
-#     return BaseMovie(**new_movie.model_dump())
+def list_all_movies() -> list[BaseMovie]:
+    return storage.get()
 
 
 @router.post(
@@ -37,6 +19,5 @@ def list_all_movies():
     response_model=BaseMovie,
     status_code=status.HTTP_201_CREATED,
 )
-def create_movie(movie_create: BaseMovie) -> BaseMovie:
-    new_movie = BaseMovie(**movie_create.model_dump())
-    return new_movie
+def create_movie(movie_create_new: CreateMovie) -> BaseMovie:
+    return storage.create(movie_create_new)
