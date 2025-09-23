216813853ff7b1ac9e1e2515c8129da203c7c86c
Irina <Irina-prokopova.style@yandex.ru>
2025-09-23 20:34:31 +0300

    Create delete_movie by_slug with status.HTTP_404_NOT_FOUND

--- catalog/api/api_v1/mouvie_a/views.py
+++ catalog/api/api_v1/mouvie_a/views.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter, status, Form
+from typing import Annotated
+
+from fastapi import APIRouter, status, Depends
 
 from api.api_v1.mouvie_a.crud import storage
+from api.api_v1.mouvie_a.dependencies import read_movie
 from schemas.movie import BaseMovie, CreateMovie
 
 router = APIRouter(
@@ -21,3 +24,25 @@ def list_all_movies() -> list[BaseMovie]:
 )
 def create_movie(movie_create_new: CreateMovie) -> BaseMovie:
     return storage.create(movie_create_new)
+
+
+@router.delete(
+    "/{slug}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    responses={
+        status.HTTP_404_NOT_FOUND: {
+            "description": "Short URL not found",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "URL 'slug' not found",
+                    }
+                },
+            },
+        },
+    },
+)
+def delete_movie_by_slug(
+    movie: Annotated[BaseMovie, Depends(read_movie)],
+) -> None:
+    storage.delete_by_slug(slug=movie.slug)
