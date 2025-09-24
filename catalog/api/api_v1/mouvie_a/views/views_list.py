1bbc87baf9a5683105112ddfbe7d5c575146bc2e
Irina <Irina-prokopova.style@yandex.ru>
2025-09-24 11:59:09 +0300

    Create views_list.py for separation logic list create views

---
+++ catalog/api/api_v1/mouvie_a/views/views_list.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, status
+
+from api.api_v1.mouvie_a.crud import storage
+from schemas.movie import BaseMovie, CreateMovie
+
+
+router = APIRouter(
+    prefix="/movies",
+    tags=["Movies_list"],
+)
+
+
+@router.get("/list_movies/", response_model=list[BaseMovie])
+def list_all_movies() -> list[BaseMovie]:
+    return storage.get()
+
+
+@router.post(
+    "/create_movie/",
+    response_model=BaseMovie,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_movie(movie_create_new: CreateMovie) -> BaseMovie:
+    return storage.create(movie_create_new)
