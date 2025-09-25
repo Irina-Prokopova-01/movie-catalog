4e2bf03c085831c17946d3155e0699a3eda1c1ce
Irina <Irina-prokopova.style@yandex.ru>
2025-09-25 13:32:02 +0300

    Create def update_partial for our storage

--- catalog/api/api_v1/mouvie_a/crud.py
+++ catalog/api/api_v1/mouvie_a/crud.py
@@ -1,30 +1,43 @@
 from pydantic import BaseModel
 
-from schemas.movie import BaseMovie, CreateMovie, UpdateMovie
+from schemas.movie import (
+    BaseMovie,
+    CreateMovie,
+    UpdateMovie,
+    UpdatePartialMovie,
+    Movie,
+)
 
 
 class Storage(BaseModel):
-    slug_movies: dict[str, BaseMovie] = {}
+    slug_movies: dict[str, Movie] = {}
 
-    def get(self) -> list[BaseMovie]:
+    def get(self) -> list[Movie]:
         return list(self.slug_movies.values())
 
-    def get_by_slug(self, movie_slug) -> BaseMovie | None:
+    def get_by_slug(self, movie_slug) -> Movie | None:
         return self.slug_movies.get(movie_slug)
 
-    def create(self, movie_create_new: CreateMovie) -> BaseMovie:
-        new_movie = BaseMovie(**movie_create_new.model_dump())
+    def create(self, movie_create_new: CreateMovie) -> Movie:
+        new_movie = Movie(**movie_create_new.model_dump())
         self.slug_movies[new_movie.slug] = new_movie
         return new_movie
 
     def delete_by_slug(self, slug) -> None:
         self.slug_movies.pop(slug, None)
 
-    def delete(self, movie_delete: BaseMovie) -> None:
+    def delete(self, movie_delete: Movie) -> None:
         self.delete_by_slug(slug=movie_delete.slug)
 
-    def update(self, movie_base: BaseMovie, movie_update: UpdateMovie) -> BaseMovie:
+    def update(self, movie_base: Movie, movie_update: UpdateMovie) -> Movie:
         for k, v in movie_update:
+            setattr(movie_base, k, v)
+        return movie_base
+
+    def update_partial(
+        self, movie_base: Movie, movie_update_in: UpdatePartialMovie
+    ) -> Movie:
+        for k, v in movie_update_in.model_dump(exclude_unset=True).items():
             setattr(movie_base, k, v)
         return movie_base
 
@@ -37,7 +50,6 @@ storage.create(
         title="Movie 3",
         description="Movie description3",
         year=2025,
-        rating=4.8,
     )
 )
 storage.create(
@@ -46,6 +58,5 @@ storage.create(
         title="Movie 2",
         description="Movie description2",
         year=2024,
-        rating=4.7,
     )
 )
