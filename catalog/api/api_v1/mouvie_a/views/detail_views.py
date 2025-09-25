efef03d6bdce2c4098ec289865ad56f8397bbbe5
Irina <Irina-prokopova.style@yandex.ru>
2025-09-25 11:15:28 +0300

    Create @router.put def update_movie

--- catalog/api/api_v1/mouvie_a/views/detail_views.py
+++ catalog/api/api_v1/mouvie_a/views/detail_views.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, status, Depends
 
 from api.api_v1.mouvie_a.crud import storage
 from api.api_v1.mouvie_a.dependencies import read_movie
-from schemas.movie import BaseMovie
+from schemas.movie import BaseMovie, UpdateMovie
 
 router = APIRouter(
     prefix="/movies",
@@ -22,12 +22,28 @@ router = APIRouter(
     },
 )
 
+MOVIE_DEP = Annotated[BaseMovie, Depends(read_movie)]
+
 
 @router.delete(
     "/{slug}",
     status_code=status.HTTP_204_NO_CONTENT,
 )
 def delete_movie_by_slug(
-    movie: Annotated[BaseMovie, Depends(read_movie)],
+    movie: MOVIE_DEP,
 ) -> None:
     storage.delete_by_slug(slug=movie.slug)
+
+
+@router.put(
+    "/{slug}",
+    response_model=BaseMovie,
+)
+def update_movie(
+    movie: MOVIE_DEP,
+    movie_update: UpdateMovie,
+):
+    return storage.update(
+        movie_base=movie,
+        movie_update=movie_update,
+    )
