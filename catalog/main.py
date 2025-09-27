92cb530a635ee7e1e57a27608f1c28035008bdec
Irina <Irina-prokopova.style@yandex.ru>
2025-09-27 20:13:36 +0300

    Add logging

--- catalog/main.py
+++ catalog/main.py
@@ -1,8 +1,16 @@
+import logging
+
 from fastapi import FastAPI, HTTPException, status
 from starlette.requests import Request
 
 from api import router as api_router
+from core import config
 
+
+logging.basicConfig(
+    level=config.LOG_LEVEL,
+    format=config.LOG_FORMAT,
+)
 app = FastAPI(
     title="Movie Catalog",
 )
