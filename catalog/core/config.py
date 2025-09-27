891431d924278fd8e31b95494748aad72766e404
Irina <Irina-prokopova.style@yandex.ru>
2025-09-27 20:14:15 +0300

    Add MOVIE_STORAGE_FILEPATH LOG_LEVEL LOG_FORMAT

---
+++ catalog/core/config.py
@@ -0,0 +1,11 @@
+import logging
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+MOVIE_STORAGE_FILEPATH = BASE_DIR / "movie_list.json"
+
+# C:\Users\User\PycharmProjects\movie-catalog\catalog\core\config.py
+LOG_LEVEL = logging.INFO
+LOG_FORMAT: str = (
+    "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+)
