90cb44c0dfc29cecc8dd7fb47bb69752b8fd17ca
Irina <Irina-prokopova.style@yandex.ru>
2025-10-01 18:52:25 +0300

    Add in dependencies.py chek for method, and if not in UNSAFE_METHODS save_state not save

--- catalog/api/api_v1/mouvie_a/dependencies.py
+++ catalog/api/api_v1/mouvie_a/dependencies.py
@@ -1,11 +1,18 @@
 import logging
 
-from fastapi import HTTPException, status, BackgroundTasks
+from fastapi import (
+    HTTPException,
+    status,
+    BackgroundTasks,
+    Request,
+)
 
 from api.api_v1.mouvie_a.crud import storage
 from schemas.movie import Movie
 
 log = logging.getLogger(__name__)
+
+UNSAFE_METHODS = frozenset({"POST", "PUT", "PATCH", "DELETE"})
 
 
 def read_movie(slug: str) -> Movie:
@@ -20,9 +27,12 @@ def read_movie(slug: str) -> Movie:
 
 def save_storage_state(
     background_tasks: BackgroundTasks,
+    request: Request,
 ):
+    log.info("Incoming %r request", request.method)
     # код выполняемый до
     log.info("first time inside dependency save_storage_state")
     yield
-    log.info("Add BackgroundTasks to save_storage")
-    background_tasks.add_task(storage.save_state)
+    if request.method in UNSAFE_METHODS:
+        log.info("Add BackgroundTasks to save_storage")
+        background_tasks.add_task(storage.save_state)
