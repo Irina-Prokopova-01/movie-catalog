8b84ac38641006ca49dae60ad379beb2239b6ff6
Irina <Irina-prokopova.style@yandex.ru>
2025-10-19 15:44:38 +0300

    Add 1 simple code for check connect to redis

---
+++ catalog/stuff.py
@@ -0,0 +1,31 @@
+from redis import Redis
+
+from core import config
+
+
+redis = Redis(
+    host=config.REDIS_HOST,
+    port=config.REDIS_PORT,
+    db=config.REDIS_DB,
+    decode_responses=True,
+)
+
+
+def main():
+    print(redis.ping())
+    redis.set("name", "Andrey")
+    redis.set("foo", "Star")
+    redis.set("number", "43")
+    print(
+        [
+            redis.get("name"),
+            redis.get("foo"),
+            redis.get("spam"),
+        ]
+    )
+    redis.delete("name")
+    print(redis.get("name"))
+
+
+if __name__ == "__main__":
+    main()
