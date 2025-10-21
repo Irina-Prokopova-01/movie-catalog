e5086ac1f30c09f74ec6f9b19d319a8ab7ae29ae
Irina <Irina-prokopova.style@yandex.ru>
2025-10-21 21:29:39 +0300

    Create package auth and services for relocate redis.py (redis_token_helper token_helper)

---
+++ catalog/api/api_v1/auth/services/redis_token_helper.py
@@ -0,0 +1,35 @@
+from redis import Redis
+
+from api.api_v1.auth.services.tokens_helper import AbstractTokensHelper
+from core import config
+
+
+class RedisTokensHelper(AbstractTokensHelper):
+    def __init__(
+        self,
+        host: str,
+        port: int,
+        db: int,
+        token_set_name: str,
+    ) -> None:
+        self.redis = Redis(
+            host=host,
+            port=port,
+            db=db,
+            decode_responses=True,
+        )
+        self.token_set = token_set_name
+
+    def token_exists(self, token: str) -> bool:
+        return bool(self.redis.sismember(self.token_set, token))
+
+    def add_token(self, token: str) -> None:
+        self.redis.sadd(self.token_set, token)
+
+
+redis_tokens = RedisTokensHelper(
+    host=config.REDIS_HOST,
+    port=config.REDIS_PORT,
+    db=config.REDIS_DB_TOKENS,
+    token_set_name=config.REDIS_TOKENS_SET_NAME,
+)
