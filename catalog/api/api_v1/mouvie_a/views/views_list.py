23ba8acf27cacd0891884052f65bf7bb81ed9782
Irina <Irina-prokopova.style@yandex.ru>
2025-10-23 19:28:55 +0300

    24.11 refactor list_views.py for can not overwrite movie wich in redis DB

--- catalog/api/api_v1/mouvie_a/views/views_list.py
+++ catalog/api/api_v1/mouvie_a/views/views_list.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, BackgroundTasks, Depends
+from fastapi import APIRouter, status, BackgroundTasks, Depends, HTTPException
 
 from api.api_v1.mouvie_a.crud import storage
 from api.api_v1.mouvie_a.dependencies import (
@@ -35,5 +35,10 @@ def create_movie(
     # background_tasks: BackgroundTasks,
     # _=Depends(api_token_required,),
 ) -> BaseMovie:
+    if not storage.get_by_slug(movie_create_new):
+        return storage.create(movie_create_new)
+    raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail=f"Short URL with slug={movie_create_new.slug!r} already exists",
+    )
     # background_tasks.add_task(storage.save_state)
-    return storage.create(movie_create_new)
