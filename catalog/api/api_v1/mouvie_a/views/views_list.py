bdf5b56136532a32d6e96f0ae35f82e5040d2cb9
Irina <Irina-prokopova.style@yandex.ru>
2025-10-24 10:50:52 +0300

    24.12 Relocate and chek on exists movie in DB (use def create_or_raise_if_exists)

--- catalog/api/api_v1/mouvie_a/views/views_list.py
+++ catalog/api/api_v1/mouvie_a/views/views_list.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status, BackgroundTasks, Depends, HTTPException
 
-from api.api_v1.mouvie_a.crud import storage
+from api.api_v1.mouvie_a.crud import storage, MovieAlreadyExistsError
 from api.api_v1.mouvie_a.dependencies import (
     # api_token_required_for_unsafe_methods,
     user_basic_auth_required_for_unsafe_methods,
@@ -44,13 +44,11 @@ def list_all_movies() -> list[BaseMovie]:
 )
 def create_movie(
     movie_create_new: CreateMovie,
-    # background_tasks: BackgroundTasks,
-    # _=Depends(api_token_required,),
 ) -> BaseMovie:
-    if not storage.get_by_slug(movie_create_new):
-        return storage.create(movie_create_new)
-    raise HTTPException(
-        status_code=status.HTTP_409_CONFLICT,
-        detail=f"Movie URL with slug={movie_create_new.slug!r} already exists",
-    )
-    # background_tasks.add_task(storage.save_state)
+    try:
+        return storage.create_or_raise_if_exists(movie_create_new)
+    except MovieAlreadyExistsError:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Movie URL with slug={movie_create_new.slug!r} already exists",
+        )
