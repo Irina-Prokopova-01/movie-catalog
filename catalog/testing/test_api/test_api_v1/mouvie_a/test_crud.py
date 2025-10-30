1545aa5f2bc8b0540c84fcacabc11941e3fde3f4
Irina <Irina-prokopova.style@yandex.ru>
2025-10-30 13:50:24 +0300

    28.3 Add package testing and example test in crud

---
+++ catalog/testing/test_api/test_api_v1/mouvie_a/test_crud.py
@@ -0,0 +1,16 @@
+# import random
+# from unittest import TestCase
+#
+#
+# def total(a: int, b: int) -> int:
+#     return a + b
+#
+#
+# class TotalTestCase(TestCase):
+#     def test_total(self) -> None:
+#         num_a = random.randint(1, 100)
+#         num_b = random.randint(1, 100)
+#         result = total(num_a, num_b)
+#         # print("result:", result)
+#         exported_result = num_a + num_b
+#         self.assertEqual(exported_result, result)
