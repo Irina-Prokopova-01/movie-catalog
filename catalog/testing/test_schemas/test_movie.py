f7c98399891d8917470654ca861fe8030f97117d
Irina <Irina-prokopova.style@yandex.ru>
2025-10-31 12:29:43 +0300

    28.4(5) Add test: 1) test_short_url_can_be_update_from_schemas 2)test_short_url_can_be_update_partial_from_schemas 3) test_short_url_create_accepts_different_urls

--- catalog/testing/test_schemas/test_movie.py
+++ catalog/testing/test_schemas/test_movie.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 
-from schemas.movie import CreateMovie, Movie
+from dns.update import Update
+
+from schemas.movie import CreateMovie, Movie, UpdateMovie, UpdatePartialMovie
 
 
 class MovieCreateTestCase(TestCase):
@@ -30,3 +32,71 @@ class MovieCreateTestCase(TestCase):
             movie_out.year,
             movie_in.year,
         )
+
+    def test_movie_can_be_update_from_schemas(self):
+        movie_in = UpdateMovie(
+            description="Some description",
+            title="Some title",
+            year=1999,
+        )
+        movie_out = Movie(
+            slug="some-slug",
+            **movie_in.model_dump(),
+        )
+        self.assertEqual(
+            movie_out.description,
+            movie_in.description,
+        )
+        self.assertEqual(
+            movie_out.title,
+            movie_in.title,
+        )
+        self.assertEqual(
+            movie_out.year,
+            movie_in.year,
+        )
+
+    def test_movie_create_accepts_different_urls(self) -> None:
+        movie_in = UpdatePartialMovie(
+            title="",
+            description="Some description",
+            year=1999,
+        )
+        movie_out = Movie(
+            slug="some-slug",
+            **movie_in.model_dump(),
+        )
+        self.assertEqual(
+            movie_out.title,
+            movie_in.title,
+        )
+        self.assertEqual(
+            movie_out.description,
+            movie_in.description,
+        )
+        self.assertEqual(
+            movie_out.year,
+            movie_in.year,
+        )
+
+    def test_movie_create_accepts_different_urls(self) -> None:
+        title_movies = [
+            "movie1",
+            "movie2",
+            # "Movie about history (Russia, Siberia)",
+            "WWW.movie.ru",
+            "movie/",
+            "rtmps://video.example.com",
+        ]
+        for title in title_movies:
+            with self.subTest(title=title, msg=f"test-title {title}"):
+                movie_in = CreateMovie(
+                    slug="some-slug",
+                    description="Some description",
+                    title=title,
+                    year=1999,
+                )
+                self.assertEqual(
+                    title,
+                    movie_in.model_dump(mode="json")["title"],
+                )
