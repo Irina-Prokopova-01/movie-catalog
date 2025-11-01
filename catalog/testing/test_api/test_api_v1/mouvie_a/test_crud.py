e8213d89f817e5c587d532d3fdbfd7246093ebaf
Irina <Irina-prokopova.style@yandex.ru>
2025-11-01 18:29:03 +0300

    28.8 Add check if getenv("TESTING") != "1":     raise OSError(    "Environment is not ready for testing"     )

--- catalog/testing/test_api/test_api_v1/mouvie_a/test_crud.py
+++ catalog/testing/test_api/test_api_v1/mouvie_a/test_crud.py
@@ -1,16 +1,24 @@
-# import random
-# from unittest import TestCase
-#
-#
-# def total(a: int, b: int) -> int:
-#     return a + b
-#
-#
-# class TotalTestCase(TestCase):
-#     def test_total(self) -> None:
-#         num_a = random.randint(1, 100)
-#         num_b = random.randint(1, 100)
-#         result = total(num_a, num_b)
-#         # print("result:", result)
-#         exported_result = num_a + num_b
-#         self.assertEqual(exported_result, result)
+import random
+from unittest import TestCase
+
+
+from os import getenv
+
+if getenv("TESTING") != "1":
+    raise OSError(
+        "Environment is not ready for testing",
+    )
+
+
+def total(a: int, b: int) -> int:
+    return a + b
+
+
+class TotalTestCase(TestCase):
+    def test_total(self) -> None:
+        num_a = random.randint(1, 100)
+        num_b = random.randint(1, 100)
+        result = total(num_a, num_b)
+        # print("result:", result)
+        exported_result = num_a + num_b
+        self.assertEqual(exported_result, result)
