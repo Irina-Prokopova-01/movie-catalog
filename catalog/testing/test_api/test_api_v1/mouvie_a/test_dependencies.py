e9089792703ed81f8a6e1790c852177e8eb18f0c
Irina <Irina-prokopova.style@yandex.ru>
2025-11-07 12:33:43 +0300

    30.4 Add in test_dependencies class TestUnsafeMethods and def test_all_methods_are_upper

--- catalog/testing/test_api/test_api_v1/mouvie_a/test_dependencies.py
+++ catalog/testing/test_api/test_api_v1/mouvie_a/test_dependencies.py
@@ -1,10 +1,14 @@
 from api.api_v1.mouvie_a.dependencies import UNSAFE_METHODS
 
 
-def test_unsafe_methods_doesnt_contain_unsafe_methods() -> None:
-    safe_methods = {
-        "GET",
-        "HEAD",
-        "OPTIONS",
-    }
-    assert not UNSAFE_METHODS & safe_methods
+class TestUnsafeMethods:
+    def test_unsafe_methods_doesnt_contain_unsafe_methods(self) -> None:
+        safe_methods = {
+            "GET",
+            "HEAD",
+            "OPTIONS",
+        }
+        assert not UNSAFE_METHODS & safe_methods
+
+    def test_all_methods_are_upper(self) -> None:
+        assert all(methods.isupper() for methods in UNSAFE_METHODS)
