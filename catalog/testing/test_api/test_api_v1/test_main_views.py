2ae7caa1f9d6f599963c97d55129cc3bbc7e1ff4
Irina <Irina-prokopova.style@yandex.ru>
2025-11-08 22:29:46 +0300

    30.8 Add in test_views.py def test_root_view()

---
+++ catalog/testing/test_api/test_api_v1/test_main_views.py
@@ -0,0 +1,16 @@
+from fastapi import status
+from starlette.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_root_view() -> None:
+    name = "John"
+    query = {"name": name}
+    response = client.get("/", params=query)
+    print(type(response))
+    assert response.status_code == status.HTTP_200_OK, response.text
+    response_data = response.json()
+    expected_message = f"Hello {name}"
+    assert response_data["message"] == expected_message, response_data
