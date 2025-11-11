46c4f0249ba22bcf7a5f3bf996fc019190462148
Irina <Irina-prokopova.style@yandex.ru>
2025-11-11 12:45:10 +0300

    30.14 Add in conftest.py auth_client and auth_token for def test_create_movie in test_list_views

--- catalog/testing/test_api/test_api_v1/conftest.py
+++ catalog/testing/test_api/test_api_v1/conftest.py
@@ -3,10 +3,28 @@ from collections.abc import Generator
 import pytest
 from fastapi.testclient import TestClient
 
+from api.api_v1.auth.services import redis_tokens
+from api.api_v1.mouvie_a.crud import storage
 from main import app
 
 
 @pytest.fixture()
 def client() -> Generator[TestClient, None, None]:
     with TestClient(app) as client:
+        yield client
+
+
+@pytest.fixture()
+def auth_token() -> Generator[str]:
+    token = redis_tokens.generate_and_save_token()
+    yield token
+    redis_tokens.delete_token(token)
+
+
+@pytest.fixture()
+def auth_client(
+    auth_token: str,
+) -> Generator[TestClient]:
+    headers = {"Authorization": f"Bearer {auth_token}"}
+    with TestClient(app, headers=headers) as client:
         yield client
