cf76b82e8fa68e7740eb76da36a4a0394c6d63f6
Irina <Irina-prokopova.style@yandex.ru>
2025-11-12 11:15:32 +0300

    30.15 Add def test_delete in test_delete.py with paramz pytest.fixture

---
+++ catalog/testing/test_api/test_api_v1/mouvie_a/test_views/test_detail_views/test_delete.py
@@ -0,0 +1,48 @@
+import random
+import string
+
+import pytest
+from _pytest.fixtures import SubRequest
+from starlette import status
+from starlette.requests import Request
+from starlette.testclient import TestClient
+
+from api.api_v1.mouvie_a.crud import storage
+from main import app
+from schemas.movie import Movie, CreateMovie
+from testing.test_api.test_api_v1.conftest import auth_client
+
+
+def create_movie(slug: str) -> Movie:
+    movie_create_new = CreateMovie(
+        slug=slug,
+        title="Movie Title",
+        description="Movie Description",
+        year=1999,
+    )
+    return storage.create(movie_create_new)
+
+
+@pytest.fixture(
+    params=[
+        "some-slug",
+        "slug",
+        pytest.param("abs", id="minimal-slug"),
+        pytest.param("qwerty-foo", id="max-slug"),
+    ],
+)
+
+def movie(request: SubRequest) -> Movie:
+    print(type(request))
+    return create_movie(request.param)
+
+
+def test_delete_by_slug(
+    auth_client: TestClient,
+    movie: Movie,
+) -> None:
+    url = app.url_path_for("delete_movie", slug=movie.slug)
+
+    response = auth_client.delete(url)
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert not storage.exists(movie.slug)
