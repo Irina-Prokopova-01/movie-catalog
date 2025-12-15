578c3db6b591ce272b8bc72d216c0138edee3131
Irina <Irina-prokopova.style@yandex.ru>
2025-12-15 12:05:05 -0800

    34.1 Connect home.html to main_views.py

--- catalog/main.py
+++ catalog/main.py
@@ -1,26 +1,1 @@
-import logging
-
-import uvicorn
-from fastapi import FastAPI
-
-from api import router as api_router
-from api.main_views import router as main_router
-from app_lifspan import lifespan
-from core.config import settings
-
-logging.basicConfig(
-    level=settings.logging.log_level,
-    format=settings.logging.log_format,
-    datefmt=settings.logging.date_format,
-)
-app = FastAPI(
-    title="Movie Catalog",
-    lifespan=lifespan,
-)
-
-app.include_router(api_router)
-app.include_router(main_router)
-
-
-if __name__ == "__main__":
-    uvicorn.run(app="main:app", reload=True)
+import loggingimport uvicornfrom fastapi import FastAPIfrom api import router as api_routerfrom api.main_views import router as main_routerfrom app_lifspan import lifespanfrom core.config import settingslogging.basicConfig(    level=settings.logging.log_level,    format=settings.logging.log_format,    datefmt=settings.logging.date_format,)app = FastAPI(    title="Movie Catalog",    lifespan=lifespan,)app.include_router(api_router)app.include_router(main_router)if __name__ == "__main__":    uvicorn.run(app="main:app", port=8004, reload=True)
