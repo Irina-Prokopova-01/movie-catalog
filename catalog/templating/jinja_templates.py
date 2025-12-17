15091922d902955f75b1147c35091101f0792c7a
Irina <Irina-prokopova.style@yandex.ru>
2025-12-16 19:44:04 -0800

    34.6 Create context global in jinja_templates.py

--- catalog/templating/jinja_templates.py
+++ catalog/templating/jinja_templates.py
@@ -1,7 +1,20 @@
+from datetime import date, datetime
+
 from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
 
 from core.config import BASE_DIR
 
+def jinja_context(
+    request: Request,
+)-> dict[str, date]:
+    return {
+        "today": date.today(),
+        "now": datetime.now(),
+
+    }
+
 templates = Jinja2Templates(
-    directory=BASE_DIR / "templates"
+    directory=BASE_DIR / "templates",
+    context_processors=[jinja_context],
 )
